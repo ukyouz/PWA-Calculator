a0cf233a8d54ac2ccf94116efb4c663017faaef2
Zhung Cheng <zhung1206@gmail.com>
2019-10-01 01:46:40 +0800

    OPCODE 0-1, 6-9 OK.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 require('./index.html')
 require('./about.html')
 import '../node_modules/@fortawesome/fontawesome-free/js/all'
+import { constants } from 'zlib'
 import('./fonts/EHSMB.ttf')
 require('./sass/main.sass')
 
 // console.log('3456');
 
-const TYPE = { BYTE: 0, WORD: 1, DWORD: 2};
-const BASE = { HEX: 0, DEC: 1, OCT: 2, BIN: 3 };
+const TYPE = { BYTE: 0, WORD: 1, DWORD: 2}
+const BASE = { HEX: 0, DEC: 1, OCT: 2, BIN: 3 }
+const OPCODE = { NONE: 0, EQUAL: 1, MOD: 2, AND: 3, OR: 4, XOR: 5, ADD: 6, SUB: 7, MUL: 8, DIV: 9 };
+const OPMODE = { IDLE: 0, AC: 1, OP: 2, NUM: 3 };
 const getEnumByIndex = (enum_name, id) => {
 	for (var k in enum_name) {
 		if (enum_name[k] == id)
@@ -21,7 +24,7 @@ const getTypeLen = type => {
 		case TYPE.DWORD: return 32;
 	}
 }
-const getValueByBase = (val_str, base) => {
+const getValueAtBase = (val_str, base) => {
 	var val = 0;
 	switch (base) {
 		case BASE.HEX: val = parseInt(val_str, 16); break;
@@ -33,7 +36,7 @@ const getValueByBase = (val_str, base) => {
 }
 const sprintf = (val_str, val_base, tar_base, type) => {
 	var len = getTypeLen(type);
-	var val = getValueByBase(val_str, val_base);
+	var val = getValueAtBase(val_str, val_base);
 	if (isNaN(val)) return "NaN";
 	if (tar_base == BASE.DEC) return val.toString(10);
 	
@@ -44,12 +47,12 @@ const sprintf = (val_str, val_base, tar_base, type) => {
 
 	return "0";
 }
-const updateLCD = (input, type, base) => {
+const updateLCD = (val_str, type, base) => {
 	var len = getTypeLen(type);
-	valueDec.innerText = sprintf(input, base, BASE.DEC, type);
-	valueHex.innerText = sprintf(input, base, BASE.HEX, type);
-	valueOct.innerText = sprintf(input, base, BASE.OCT, type);
-	var binStr = sprintf(input, base, BASE.BIN, type);
+	valueDec.innerText = sprintf(val_str, base, BASE.DEC, type);
+	valueHex.innerText = sprintf(val_str, base, BASE.HEX, type);
+	valueOct.innerText = sprintf(val_str, base, BASE.OCT, type);
+	var binStr = sprintf(val_str, base, BASE.BIN, type);
 	var binString = binStr.substr(24, 8);
 	if (len > 8)
 		binString = binStr.substr(16, 8) + " " + binString;
@@ -60,12 +63,15 @@ const updateLCD = (input, type, base) => {
 
 var gEType = TYPE.DWORD;
 var gEBase = BASE.DEC;
+var gRegA = 0, gRegB = 0, gRegSum = 0, gIntMulDivCnt = 0, gEMode = OPMODE.IDLE;
+var gECodePrevPrev = OPCODE.NONE, gECodePrev = OPCODE.NONE, gECodeActive = OPCODE.NONE;
 var gStrInput = '0';
 
-var btn_type = document.querySelector('#btnType')
-var list_items = document.querySelectorAll('.list__item')
-var num_pad = document.querySelector('#numpad')
-var num_pad_btns = num_pad.querySelectorAll('.pad__btn')
+var btn_type = document.querySelector('#btnType');
+var btn_AC = document.querySelector('#btnAC');
+var list_items = document.querySelectorAll('.list__item');
+var num_pad = document.querySelector('#numpad');
+var num_pad_btns = num_pad.querySelectorAll('.pad__btn'), prev_pad_btn = null;
 
 /*
  * Type changed
@@ -79,8 +85,14 @@ btn_type.addEventListener('click', e => {
 /*
  * Base changed
  */
+const clearPadBtnState = () => {
+	num_pad_btns.forEach(elem => {
+		elem.classList.remove('is-active')
+	})
+}
 list_items.forEach((el, id) => {
 	el.addEventListener('click', e => {
+		clearPadBtnState();
 		var old_base = gEBase;
 		list_items.forEach(elem => {
 			elem.classList.remove('is-active')
@@ -88,7 +100,6 @@ list_items.forEach((el, id) => {
 		el.classList.add('is-active')
 		gEBase = id;
 		gStrInput = sprintf(gStrInput, old_base, gEBase, gEType);
-		console.log(gStrInput,old_base,getEnumByIndex(BASE, gEBase));
 		num_pad_btns.forEach(el => {
 			if ((gEBase > BASE.HEX && el.dataset.tag.match(/^[abcdef]$/))
 				|| (gEBase > BASE.DEC && el.dataset.tag.match(/^[98]$/))
@@ -102,23 +113,186 @@ list_items.forEach((el, id) => {
 	})
 })
 
+var debug = (key) => {
+	console.log(
+		'input='+ gStrInput,
+		'"'+ key + '"',
+		'a=' + gRegA, 'b=' + gRegB, 'sum=' + gRegSum,
+		'OP_PP:'+ getEnumByIndex(OPCODE, gECodePrevPrev),
+		'OP_P:' + getEnumByIndex(OPCODE, gECodePrev),
+		'OP_A:' + getEnumByIndex(OPCODE, gECodeActive),
+		'gEMode:' + getEnumByIndex(OPMODE, gEMode));
+}
+const ac_reset = () => {
+	gRegA = 0,
+	gRegB = 0,
+	gRegSum = 0,
+	gIntMulDivCnt = 0,
+	gEMode = OPMODE.NUM,
+	gECodePrevPrev = OPCODE.NONE,
+	gECodePrev = OPCODE.ADD,
+	gECodeActive = OPCODE.NONE,
+	gStrInput = '0';
+	prev_pad_btn = null;
+	btn_AC.innerText = "AC";
+	clearPadBtnState();
+	updateLCD(gStrInput, gEType, gEBase);
+}
+/*
+ * Event handlers
+ */
+const calculation = (opcode, op_next) => {
+	var val = getValueAtBase(gStrInput, gEBase);
+	var reg_a = 0, reg_b = 0;
+	console.log(getEnumByIndex(OPCODE, opcode), val, getEnumByIndex(OPCODE, op_next));
+	switch (opcode) {
+		case OPCODE.ADD:
+			if (op_next == OPCODE.MUL || op_next == OPCODE.DIV) {
+				gRegB = val;
+				gIntMulDivCnt++;
+			} else {
+				gRegA += val;
+				reg_a = val;
+				gIntMulDivCnt = 0;
+			}
+			break;
+		case OPCODE.SUB:
+			if (op_next == OPCODE.MUL || op_next == OPCODE.DIV) {
+				gRegB = val;
+				gIntMulDivCnt++;
+			} else {
+				gRegA -= val;
+				reg_a = val;
+				gIntMulDivCnt = 0;
+			}
+			break;
+		case OPCODE.MUL:
+			gRegB = (gIntMulDivCnt == 0) ? val : gRegB * val; 
+			gIntMulDivCnt++;
+			reg_b = val;
+			break;
+		case OPCODE.DIV:
+			gRegB = (gIntMulDivCnt == 0) ? val : parseInt(gRegB / val);
+			gIntMulDivCnt++;
+			reg_b = val;
+			break;
+	}
+	if (op_next == OPCODE.EQUAL) {
+		gRegSum = gRegA + gRegB;
+		if (opcode == OPCODE.MUL || opcode == OPCODE.DIV) {
+			gRegA = 0;
+			gRegB = gRegSum;
+		} else if (opcode == OPCODE.ADD || opcode == OPCODE.SUB) {
+			gRegA = gRegSum;
+			gRegB = 0;
+		}
+	}
+}
+const btn_AC_clicked = () => {
+	debug('ac0');
+	if (gEMode == OPMODE.AC) {
+		gEMode = OPMODE.NUM;
+		clearPadBtnState();
+		ac_reset();
+	} else if (gEMode == OPMODE.NUM) {
+		gEMode = OPMODE.AC;
+		gECodeActive = gECodePrev;
+		gECodePrev = gECodePrevPrev;
+		(prev_pad_btn) && (prev_pad_btn.classList.add('is-active'));
+		btn_AC.innerText = "AC";
+		gStrInput = "0";
+		updateLCD(gStrInput, gEType, gEBase)
+} else {
+		clearPadBtnState();
+		ac_reset();
+	}
+	debug('ac1');
+}
+const btn_modeOp_clicked = (_this) => {
+	if (gEMode != OPMODE.OP) {
+		prev_pad_btn = _this;
+		if (gEMode == OPMODE.IDLE) {
+			gIntMulDivCnt = 0;
+			if (gECodePrev == OPCODE.ADD || gECodePrev == OPCODE.SUB) {
+				gStrInput = gRegA;
+				gRegA = 0;
+			} else if (gECodePrev == OPCODE.MUL || gECodePrev == OPCODE.DIV) {
+				gStrInput = gRegB;
+				gRegB = 0;
+			}
+		}
+	} else if (gEMode == OPMODE.OP) {
+		clearPadBtnState();
+	}
+	gEMode = OPMODE.OP;
+	_this.classList.add('is-active')
+}
+const btn_PLUS_MINUS_clicked = (_this) => {
+	btn_modeOp_clicked(_this);
+	debug('+-');
+}
+const btn_MUL_DIV_clicked = (_this) => {
+	btn_modeOp_clicked(_this);
+	debug('*/');
+}
+const btn_BitOP_clicked = (_this) => {
+	btn_modeOp_clicked(_this);
+}
+const btn_NUM_clicked = (_this, key) => {
+	if (gEMode == OPMODE.NUM) {
+		gStrInput += key;
+	} else if (gEMode == OPMODE.OP || gEMode == OPMODE.AC) {
+		clearPadBtnState();
+		gECodePrevPrev = gECodePrev;
+		gECodePrev = gECodeActive;
+		gECodeActive = OPCODE.NONE;
+		if (gEMode == OPMODE.OP)
+			calculation(gECodePrevPrev, gECodePrev);
+		gEMode = OPMODE.NUM;
+		gStrInput = key;
+	} else if (gEMode == OPMODE.IDLE) {
+		ac_reset();
+		gStrInput = key;
+	}
+	btn_AC.innerText = "C";
+	debug(key);
+	updateLCD(gStrInput, gEType, gEBase);
+}
+const btn_EQUAL_clicked = (_this) => {
+	clearPadBtnState();
+	calculation(gECodePrev, OPCODE.EQUAL);
+	gEMode = OPMODE.IDLE;
+	updateLCD(gRegSum.toString(10), gEType, BASE.DEC);
+	debug('=');
+}
+
 /*
  * Keypad clicked
  */
 num_pad.addEventListener('click', e => {
-	if (!e.target.classList.contains('pad__btn') || e.target.classList.contains('is-disabled'))
+	var _this = e.target;
+	if (!_this.classList.contains('pad__btn') || _this.classList.contains('is-disabled'))
 		return;
-	var key = e.target.dataset.tag;
-	var value = getValueByBase(gStrInput, gEBase);
+	var key = _this.dataset.tag;
+	var value = getValueAtBase(gStrInput, gEBase);
 	switch (key) {
-		case "ac": gStrInput = "0"; break;
 		case "+/-": gStrInput = sprintf(-1 * value, gEBase, gEBase, gEType); break;
 		case "shr": gStrInput = sprintf(value >> 1, gEBase, gEBase, gEType); break;
 		case "shl": gStrInput = sprintf(value << 1, gEBase, gEBase, gEType); break;
 		case "~": gStrInput = sprintf(~value, gEBase, gEBase, gEType); break;
-		default: gStrInput += key;
+		case "ac": btn_AC_clicked(); break;
+		case "+": gECodeActive = OPCODE.ADD; btn_PLUS_MINUS_clicked(_this); break;
+		case "-": gECodeActive = OPCODE.SUB; btn_PLUS_MINUS_clicked(_this); break;
+		case "*": gECodeActive = OPCODE.MUL; btn_MUL_DIV_clicked(_this); break;
+		case "/": gECodeActive = OPCODE.DIV; btn_MUL_DIV_clicked(_this); break;
+		case "&": gECodeActive = OPCODE.AND; btn_BitOP_clicked(_this); break;
+		case "|": gECodeActive = OPCODE.OR; btn_BitOP_clicked(_this); break;
+		case "^": gECodeActive = OPCODE.XOR; btn_BitOP_clicked(_this); break;
+		case "%": gECodeActive = OPCODE.MOD; _this.classList.add('is-active'); break;
+		case "=": gECodeActive = OPCODE.EQUAL; btn_EQUAL_clicked(_this); break;
+		// input number
+		default: btn_NUM_clicked(_this, key); break;
 	}
-	updateLCD(gStrInput, gEType, gEBase);
 })
 
-updateLCD(gStrInput, gEType, gEBase);
+ac_reset();
